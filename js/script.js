a04c715a05104fea46bbabd64f3deaa32eb63b45
huuluantvka2 <huuluantvka2@gmail.com>
2020-07-30 21:27:41 +0700

    third update

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
     e.preventDefault();
     $('html,body').animate({ scrollTop: 0 }, 1400);
   });
+  var positionFooter = $('.page-footer').offset().top - 500;
+  positionFooter += 'px';
+  document.getElementById('pso-relative').style.height = positionFooter;
 });
